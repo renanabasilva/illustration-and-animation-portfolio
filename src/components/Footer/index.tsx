52706df79feb63b54cb826d99986da5e21fbcdcb
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-10-21 17:56:11 +0100

    feat: refactor footer code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,59 @@
 import styles from "./Footer.module.css"
 
+const socialLinks: Array<{
+  alt: string;
+  logoPath: string;
+  link: string;
+}> = [
+  {
+    alt: "LinkedIn Logo",
+    logoPath: "src/assets/icons/linkedin_icon.png",
+    link: "https://www.linkedin.com/in/juliepassanezi/",
+  },
+  {
+    alt: "Instagram Logo",
+    logoPath: "src/assets/icons/instagram_icon.png",
+    link: "https://www.instagram.com/jupm.art/",
+  },
+  {
+    alt: "Twitter Logo",
+    logoPath: "src/assets/icons/twitter_icon.png",
+    link: "https://twitter.com/jupm_art",
+  },
+  {
+    alt: "YouTube Logo",
+    logoPath: "src/assets/icons/youtube_icon.png",
+    link: "#",
+  },
+  {
+    alt: "Discord Logo",
+    logoPath: "src/assets/icons/discord_icon.png",
+    link: "#",
+  },
+];
+
 const Footer = () => {
   return(
     <footer className={styles.footerContainer}>
       <ul className={styles.social}>
-            <li><a href="https://www.linkedin.com/in/juliepassanezi/" target="_blank" rel="noreferrer"><img src="src/assets/icons/linkedin_icon.png" alt="LinkedIn Logo"/></a></li>
-            <li><a href="https://www.instagram.com/jupm.art/" target="_blank" rel="noreferrer"><img src="src/assets/icons/instagram_icon.png" alt="Instagram Logo" /></a></li>
-            <li><a href="https://twitter.com/jupm_art" target="_blank" rel="noreferrer"><img src="src/assets/icons/twitter_icon.png" alt="Twitter Logo" /></a></li>
-            <li><a href="#" target="_blank" rel="noreferrer"><img src="src/assets/icons/youtube_icon.png" alt="YouTube Logo"/></a></li>
-            <li><a href="#" target="_blank" rel="noreferrer"><img src="src/assets/icons/discord_icon.png" alt="Discord Logo"/></a></li>
-        </ul>
-        <p>Copyright © 2024 Julie Passanezi</p>
-        <p className={styles.dev}>Developed by <a href="https://github.com/renanabasilva" target="_blank" rel="noreferrer">Renan Aba</a></p>
+        {
+          socialLinks.map((social) => {
+            return(
+              <li key={social.alt}>
+                <a 
+                  href={social.link} 
+                  target="_blank" 
+                  rel="noreferrer"
+                >
+                  <img src={social.logoPath} alt={social.alt} />
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <p>Copyright © 2024 Julie Passanezi</p>
+      <p className={styles.dev}>Developed by <a href="https://github.com/renanabasilva" target="_blank" rel="noreferrer">Renan Aba</a></p>
     </footer>
   )
 }
