c7af593b59199e4a4bbf9a37c02c27855e13832e
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-11-04 12:50:58 +0000

    feat: Add interaction (next and prev) by keyboard arrows

--- src/components/ImageModal/index.tsx
+++ src/components/ImageModal/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styles from "./ImageModal.module.css"
 
 interface ModalProps {
@@ -9,6 +10,29 @@ interface ModalProps {
 }
 
 const ImageModal: React.FC<ModalProps> = ({ src, alt, onClose, onNext, onPrev }) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'ArrowRight':
+          onNext();
+          break;
+        case 'ArrowLeft':
+          onPrev();
+          break;
+        case 'Escape':
+          onClose();
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose, onNext, onPrev]);
+
   return(
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
